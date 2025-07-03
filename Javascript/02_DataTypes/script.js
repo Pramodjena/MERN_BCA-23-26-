a196d0113dfdde646d3486c9f94293d4894feba7
Pramodjena <pramoodjena@gmail.com>
2025-07-03 20:59:53 +0530

    updated

--- Javascript/02_DataTypes/script.js
+++
@@ -1,7 +0,0 @@
-// console.log(myName);
-
-// var myName;
-// myName = "LIT";
-
-console.log(myAge);
-let myAge = 25;
--- Javascript/08_Comparison/script.js
+++
@@ -1,8 +0,0 @@
-// const userAge1 = 21
-// const userAge2 = 24
-
-// let myName = prompt("Enter your name:");
-// console.log(!myName); //
-
-// let myAge = 0 + 0 * 0;
-// console.log(!myAge); //
