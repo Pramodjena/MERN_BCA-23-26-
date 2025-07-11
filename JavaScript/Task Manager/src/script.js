262d30d0b1b465f56688c0502b8ae3a0ac5701c6
Pramodjena <pramoodjena@gmail.com>
2025-07-11 14:36:14 +0530

    task manager updated

---
+++ JavaScript/Task Manager/src/script.js
@@ -0,0 +1,18 @@
+// Create an array to store task
+const tasks = [];
+
+// Dom element
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+
+// Load Tasks
+document.addEventListener("DOMContentLoaded", () => {});
+
+function addInputsFromTask() {
+  taskInput.addEventListener("keypress", (event) => {
+    // if (event.key === "Enter") {
+    //   alert("You hit enter");
+    // }
+    console.log("Okay");
+  });
+}
